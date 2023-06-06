7a374bdffcb9402e3e83360a1a4a5aac14a71a14
Lieven <schokkl1@hva.nl>
2023-06-06 17:29:43 +0200

    -reset

---
+++ Distorion/source/cids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2023 .
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace MyCompanyName {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kDistortionProcessorUID (0x8B2ACC80, 0x5F355C24, 0x81FD742C, 0xD2D22B2F);
+static const Steinberg::FUID kDistortionControllerUID (0x3FC3A1B5, 0x48E552D0, 0x847C9538, 0xF7920D48);
+
+#define DistortionVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
